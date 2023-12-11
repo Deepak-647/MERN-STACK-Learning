c00a2411dbab7bb4e58d0b42c02b3edb7554fc46
Deepak <dbehera647@gmail.com>
2023-12-11 12:57:45 +0530

    Registration & Login Form Validation using ZOD

---
+++ validators/auth-validator.js
@@ -0,0 +1,29 @@
+const {z} =require("zod");
+
+//Creating an object schema
+
+const signupSchema = z.object({
+    username : z.string({required_error : "Name is required"})
+    .trim()
+    .min(3, {message : "Name must be at least of 3 chars"})
+    .max(35, {message : "Name must be more than 35 chars"}),
+
+    email :z 
+    .string({required_error : "Email is required"})
+    .trim()
+    .email({message: "Invalid email address"})
+    .min(3,{message : "Email must be at least of 3 chars"})
+    .max(255,{message : "Email must not be more than 255 chars"}),
+
+    phone :z
+    .string({required_error : "Phone is required"})
+    .trim()
+    .min(10,{message :"Phone must be at least 10 chars"}),
+
+    password :z
+    .string({required_error : "Password must required"})
+    .min(8,{message:"password must be at least of 6 chars"})
+    
+})
+
+module.exports= signupSchema;
