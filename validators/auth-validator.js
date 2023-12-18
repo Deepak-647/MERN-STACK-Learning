3f82cd107595e7e3eacac2a5adcc618fe48b522d
Deepak <dbehera647@gmail.com>
2023-12-18 12:51:12 +0530

    Validating Login Schema Validaor

--- validators/auth-validator.js
+++ validators/auth-validator.js
@@ -1,13 +1,6 @@
 const {z} =require("zod");
 
-//Creating an object schema
-
-const signupSchema = z.object({
-    username : z.string({required_error : "Name is required"})
-    .trim()
-    .min(3, {message : "Name must be at least of 3 chars"})
-    .max(35, {message : "Name must be more than 35 chars"}),
-
+const loginSchema =z.object({
     email :z 
     .string({required_error : "Email is required"})
     .trim()
@@ -15,15 +8,24 @@ const signupSchema = z.object({
     .min(3,{message : "Email must be at least of 3 chars"})
     .max(255,{message : "Email must not be more than 255 chars"}),
 
+    password :z
+    .string({required_error : "Password must required"})
+    .min(8,{message:"password must be at least of 6 chars"})
+})
+//Creating an object schema
+
+const signupSchema = loginSchema.extend({
+    username : z.string({required_error : "Name is required"})
+    .trim()
+    .min(3, {message : "Name must be at least of 3 chars"})
+    .max(35, {message : "Name must be more than 35 chars"}),
+
     phone :z
     .string({required_error : "Phone is required"})
     .trim()
     .min(10,{message :"Phone must be at least 10 chars"}),
 
-    password :z
-    .string({required_error : "Password must required"})
-    .min(8,{message:"password must be at least of 6 chars"})
     
 })
 
-module.exports= signupSchema;
+module.exports= {signupSchema ,loginSchema};
