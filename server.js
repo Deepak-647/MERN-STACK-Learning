284fe1875f6cf31f35ed73d0cdbf05791062049f
Deepak-647 <82494501+Deepak-647@users.noreply.github.com>
2023-12-21 09:00:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const contactRoute = require("./routing/contact-router")
 //handling cors policy issue
 const corsOptions ={
   origin:["http://localhost:5173"],
-  methods:["GET,POST],
+  methods:["GET","POST"],
   credentials :true,
 }
 app.use(cors(corsOptions));
